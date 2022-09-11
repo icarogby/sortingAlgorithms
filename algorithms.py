bbb1391651a82ffbe4138482f8e78ccb5dddbcd3
Icaro <icarogabryel2001@gmail.com>
2022-09-11 16:17:53 -0300

    merge code finished

---
+++ algorithms.py
@@ -0,0 +1,67 @@
+def insertionSort(arr: list) -> list:
+    for i in range(1, len(arr)):
+        key = arr[i]
+
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+def bubbleSort(arr: list) -> list:
+    exchanges = True
+    i = len(arr) - 1
+
+    while i > 0 and exchanges:
+        exchanges = False
+
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                exchanges = True
+                
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        i -= 1
+
+    return arr
+
+# selectionsort
+
+def mergeSort(arr: list) -> list:
+    if len(arr) > 1:
+        mid = len(arr)//2
+  
+        left = arr[:mid]
+        right = arr[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+  
+        i = j = k = 0
+  
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            
+            k += 1
+  
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+  
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+    return arr
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sortingAlgorithms as alg
+from algorithms import *
 
 arr = [5, 3, 4, 2]
 
-print(alg.bubbleSort(arr))
-print(alg.insertionSort(arr))
-print(alg.mergeSort(arr, 0, len(arr)))
+print(bubbleSort(arr))
+print(insertionSort(arr))
+print(mergeSort(arr))
 
