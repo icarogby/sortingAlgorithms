dcd5e96876c0608bb90097bf4c109947fa64b985
Icaro <icarogabryel2001@gmail.com>
2022-09-11 16:37:59 -0300

    selection sort added

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,15 @@
+def selectionSort(arr: list) -> list:
+    for i in range(len(arr)):
+        min = i
+
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min]:
+                min = j
+
+        arr[i], arr[min] = arr[min], arr[i]
+
+    return arr
+
 def insertionSort(arr: list) -> list:
     for i in range(1, len(arr)):
         key = arr[i]
@@ -30,7 +42,6 @@ def bubbleSort(arr: list) -> list:
 
     return arr
 
-# selectionsort
 # shell
 
 def mergeSort(arr: list) -> list:
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from algorithms import *
 
 arr = [5, 3, 4, 2]
 
+print(selectionSort(arr))
 print(bubbleSort(arr))
 print(insertionSort(arr))
 print(mergeSort(arr))
