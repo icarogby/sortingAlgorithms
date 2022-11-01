c74d3f03f32a33106c40325fd812d537ada12026
Ícaro Gabryel <icarogabryel2001@gmail.com>
2022-11-01 17:17:38 -0300

    update

--- main.py
+++ main.py
@@ -2,23 +2,41 @@ from tests import *
 
 from matplotlib import pyplot as plt
 
+def test(funcao, o):
+    results4 = [0, 0, 0, 0]
+
+    for i in range(3):
+        temp = funcao(o)
+
+        for j in range(4):
+            results4[j] = results4[j] + temp[j]
+
+    for j in range(4):
+        results4[j] = results4[j]/3
+
+    return results4
 
 def main():
+    opc = int(input("Select: "))
+
     x = [1, 2, 3, 4, ]
 
-    Results1 = selectionTest(1)
-    Results2 = insertionTest(1)
-    Results3 = bubbleTest(1)
-    Results4 = mergeTest(1)
-    Results5 = heapTest(1)
-    Results6 = quickTest(1)
+    listatest = test(selectionTest, opc)
 
-    plt.plot(x, Results1)
-    plt.plot(x, Results2)
-    plt.plot(x, Results3)
-    plt.plot(x, Results4)
-    plt.plot(x, Results5)
-    plt.plot(x, Results6)
+    # Results1 = selectionTest(o)
+    # Results2 = insertionTest(o)
+    # Results3 = bubbleTest(o)
+
+
+    # Results5 = heapTest(o)
+    # Results6 = quickTest(o)
+
+    # plt.plot(x, Results1)
+    # plt.plot(x, Results2)
+    # plt.plot(x, Results3)
+    plt.plot(x, listatest)
+    # plt.plot(x, Results5)
+    # plt.plot(x, Results6)
 
     plt.show()
 
---
+++ test.py
@@ -0,0 +1,16 @@
+result = [1,2,3,4]
+x2 = [5,6,7,8]
+
+print(type(zip(result, x2)))
+
+def tt(funcao):
+    for i in range(3):
+        funcao("sexo")
+
+    return 14
+
+numero = tt(print)
+
+print(numero)
+
+
