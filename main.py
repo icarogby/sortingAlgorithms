1da2e6f65961ec7d5a8017db03ed82a6741de4b5
Ícaro Gabryel <91147230+icarogby@users.noreply.github.com>
2022-12-27 21:24:19 -0300

    release

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
 from tests import *
 from algorithms import *
-
-from matplotlib import pyplot as plt
+from save import *
 
 def main():
     opc = int(input("1) Aleatory\n2) Ascending\n3) Descending\n\nSelect: "))
 
-    x = [1, 2, 3, 4, 5, 6]
-
     selectionY = mediaTimeTest(selectionSort, opc)
     insertionY = mediaTimeTest(insertionSort, opc)
-    bubleY = mediaTimeTest(bubbleSort, opc)
-    mergeY = mediaTimeTest(mergeSort, opc)
-    heapY = mediaTimeTest(heapSort, opc)
-    quickY = mediaTimeTest(quickSort, opc)
+    bubbleY    = mediaTimeTest(bubbleSort, opc)
+    mergeY     = mediaTimeTest(mergeSort, opc)
+    heapY      = mediaTimeTest(heapSort, opc)
+    quickY     = mediaTimeTest(quickSort, opc)
 
-    plt.plot(x, selectionY, color = 'r', label = "Selection Sort")
-    plt.plot(x, insertionY, color = 'b', label = "Insertion Sort")
-    plt.plot(x, bubleY, color = 'y', label = "Bubble Sort")
-    plt.plot(x, mergeY, color = 'c', label = "Merge Sort")
-    plt.plot(x, heapY, color = 'm', label = "Heap Sort")
-    plt.plot(x, quickY, color = 'g', label = "Quick Sort")
-
-    plt.title("Sorting Algorithms")
-    plt.xlabel("Number of Elements")
-    plt.ylabel("Media of time (seconds)")
-
-    plt.grid(True)
-    plt.yscale("log")
-    plt.legend()
-    plt.savefig("plottest2ale.png")
+    saveCSV(selectionY, insertionY, bubbleY, mergeY, heapY, quickY, opc)
+    savePNG(selectionY, insertionY, bubbleY, mergeY, heapY, quickY, opc)
     
 if __name__ == "__main__":
     main()
---
+++ save.py
@@ -0,0 +1,84 @@
+from matplotlib import pyplot as plt
+
+def saveCSV(list1, list2, list3, list4, list5, list6, opc):
+    try:
+        if opc == 1:
+            file = open(f'aleatory.csv', 'w')
+        elif opc == 2:
+            file = open(f'ascending.csv', 'w')
+        elif opc == 3:
+            file = open(f'descending.csv', 'w')
+
+        file.write("Algorithm/Number of Elements, 512, 1024, 2048, 4096, 8192, 16384\n")
+        
+        file.write(f"Selection Sort, ")
+        for i in range(len(list1)):
+            if (i == len(list1) - 1):
+                file.write(f"{list1[i]}\n")
+            else:
+                file.write(f"{list1[i]}, ")
+
+        file.write(f"Insertion Sort, ")
+        for i in range(len(list2)):
+            if (i == len(list2) - 1):
+                file.write(f"{list2[i]}\n")
+            else:
+                file.write(f"{list2[i]}, ")
+
+        file.write(f"Bubble Sort, ")
+        for i in range(len(list3)):
+            if (i == len(list3) - 1):
+                file.write(f"{list3[i]}\n")
+            else:
+                file.write(f"{list3[i]}, ")
+
+        file.write(f"Merge Sort, ")
+        for i in range(len(list4)):
+            if (i == len(list4) - 1):
+                file.write(f"{list4[i]}\n")
+            else:
+                file.write(f"{list4[i]}, ")
+
+        file.write(f"Heap Sort, ")
+        for i in range(len(list5)):
+            if (i == len(list5) - 1):
+                file.write(f"{list5[i]}\n")
+            else:
+                file.write(f"{list5[i]}, ")
+        
+        file.write(f"Quick Sort, ")
+        for i in range(len(list6)):
+            if (i == len(list6) - 1):
+                file.write(f"{list6[i]}\n")
+            else:
+                file.write(f"{list6[i]}, ") 
+    except:
+        print("file fail.")
+    else:
+        file.close()
+
+def savePNG(selectionY, insertionY, bubbleY, mergeY, heapY, quickY, opc):
+    x = [512, 1024, 2048, 4096, 8192, 16384]
+    
+    plt.plot(x, selectionY, color = 'r', label = "Selection Sort")
+    plt.plot(x, insertionY, color = 'b', label = "Insertion Sort")
+    plt.plot(x, bubbleY, color = 'y', label = "Bubble Sort")
+    plt.plot(x, mergeY, color = 'c', label = "Merge Sort")
+    plt.plot(x, heapY, color = 'm', label = "Heap Sort")
+    plt.plot(x, quickY, color = 'g', label = "Quick Sort")
+
+    plt.title("Sorting Algorithms")
+    plt.xlabel("Number of Elements")
+    plt.ylabel("Media of time (seconds)")
+
+    plt.grid(True)
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.legend()
+
+    if opc == 1:
+        plt.savefig("aleatory.png")
+    elif opc == 2:
+        plt.savefig("ascending.png")
+    elif opc == 3:
+        plt.savefig("descending.png")
